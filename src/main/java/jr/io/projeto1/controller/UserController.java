efbd1fe22674e9e0f1ede196673c6198ce1e7146
Edvaldo Junior <edvaldo1jr@gmail.com>
2021-01-22 20:17:13 -0300

    MVC
    Usando MVC

---
+++ src/main/java/jr/io/projeto1/controller/UserController.java
@@ -0,0 +1,24 @@
+package jr.io.projeto1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jr.io.projeto1.repository.UserRepository;
+
+@Controller
+public class UserController {
+
+	private UserRepository userRepository;
+	
+	public UserController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+	
+	@RequestMapping("/user")
+	public String getUsers(Model model) {
+		model.addAttribute("userList", this.userRepository.findAll());
+		
+		return "user";
+	}
+}
